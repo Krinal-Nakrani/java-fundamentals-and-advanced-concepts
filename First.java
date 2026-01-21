8111408b7e6efe36d1b5dfddda4a6893dbdb358a
Krinal-Nakrani <104360261+Krinal-Nakrani@users.noreply.github.com>
2026-01-21 12:16:28 +0530

    basics of Applet

---
+++ First.java
@@ -0,0 +1,18 @@
+//First.java  
+import java.applet.Applet;  
+import java.awt.Graphics;  
+
+public class First extends Applet
+{  
+  
+	public void paint(Graphics g)
+	{  
+		g.drawString("welcome to applet",150,150);  
+	}  
+  
+} 
+//Html code.... 
+/* 
+<applet code="First.class" width="300" height="300"> 
+</applet> 
+*/  
